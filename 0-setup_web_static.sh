7b344460098ef8ca2960a368367ca89f72d4bacc
Osama <elsharqawy2@gmail.com>
2024-05-03 21:50:58 +0300

    Web Static Deploy

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,42 +1,30 @@
 #!/usr/bin/env bash
-# a Bash script that sets up our web servers for the deploments of web_static.
+# setup my webservers.
 
 sudo apt-get update
 sudo apt-get install -y nginx
-
-sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-
-sudo echo -e "<!DOCTYPE html>
-<html>
-  <head>
-  </head>
-  <body>
-    <p>Hello World</p>
-  </body>
-</html>" | tee /data/web_static/releases/test/index.html
-
+sudo mkdir -p /data/web_static/shared/
+sudo sh -c 'echo "Html Simple File!" >/data/web_static/releases/test/index.html'
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo printf %s "server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-        add_header X-Served-By $HOSTNAME;
-        root   /var/www/html;
-        index  index.html index.htm;
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-        location /hbnb_static {
-                alias /data/web_static/current;
-                index index.html index.htm;
-        }
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
 
-        error_page 404 /404.html;
-        location /404 {
-                root /var/www/html;
-                internal;
-        }
-}" | sudo tee -a /etc/nginx/sites-available/default
-
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 sudo service nginx restart
