d0e5992dc003a0ebdca1c3fa4faee0140fb814f7
Osama <elsharqawy2@gmail.com>
2024-05-03 23:41:27 +0300

    Web Static Deploy

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
+""" a fabrit script to create backup archive."""
 
 from fabric.api import local
 from datetime import datetime
 
-""" a Fabric script that generates a tgz archive
-from the contents of web_static.
-"""
-
 
 def do_pack():
     """pack an archive"""
-
     try:
-        local("mkdir -p versions")
-        curDt = str(datetime.now().strftime("%Y%m%d%H%M%S"))
+        curDt = datetime.now().strftime("%Y%m%d%H%M%S")
         fileName = f"web_static_{curDt}.tgz"
         fullPath = f"versions/{fileName}"
-        local(f"tar -cvzf {fullPath}")
+        local('mkdir -p versions')
+        local(f'tar -cvzf {fullPath}')
         return (fullPath)
     except Exception:
         return None
