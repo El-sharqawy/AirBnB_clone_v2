6c7dc43c48551d8fe95fff1e5062258ec977aece
Osama <elsharqawy2@gmail.com>
2024-05-03 21:40:49 +0300

    Web Static Deploy

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,15 +4,39 @@
 sudo apt-get update
 sudo apt-get install -y nginx
 
-sudo mkdir -p /data/web_static/shared
-sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 
-sudo sh -c 'echo -e "<!DOCTYPE html>\n<html>\n<body>\n<p>Hello World</p>\n</body>\n</html>" > data/web_static/releases/test/index.html'
+sudo echo -e "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <p>Hello World</p>
+  </body>
+</html>" | tee /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-sudo chown -R ubuntu:ubuntu /data
+sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+sudo printf %s "server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        add_header X-Served-By $HOSTNAME;
+        root   /var/www/html;
+        index  index.html index.htm;
+
+        location /hbnb_static {
+                alias /data/web_static/current;
+                index index.html index.htm;
+        }
+
+        error_page 404 /404.html;
+        location /404 {
+                root /var/www/html;
+                internal;
+        }
+}" > /etc/nginx/sites-available/default
 
 sudo service nginx restart
