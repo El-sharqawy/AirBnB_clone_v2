a2f4fd432fac0f62b58a692e7c6e6387d2211801
El-sharqawy <elsharqawy2@gmail.com>
2024-05-18 21:57:36 +0300

    Hello HBNB!

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+start Flask web Application
+"""
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route("/statis_list", strict_slashes=False)
+def statesList():
+    """Display HTML page with list of states sorted"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """close storage on teardown"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
