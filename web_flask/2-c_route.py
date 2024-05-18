4bf03ce7073d56dab05789a9654b8df3030f6c27
El-sharqawy <elsharqawy2@gmail.com>
2024-05-18 15:11:48 +0000

    Hello HBNB!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -22,7 +22,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
     """Display "C" Followed by the given text"""
-    return 'C ' + text.replace('_', '')
+    return 'C ' + text.replace('_', ' ')
 
 
 if __name__ == '__main__':
