50fba22301dd70b599a05e9dbaef2bf2178ed56c
El-sharqawy <elsharqawy2@gmail.com>
2024-05-18 21:18:21 +0300

    New Engine

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -16,6 +16,15 @@ from models.base_model import Base
 from hashlib import md5
 import os
 
+MyClasses = {
+    "Amenity": Amenity,
+    "City": City,
+    "Place": Place,
+    "Review": Review,
+    "State": State,
+    "User": User,
+}
+
 
 class DBStorage:
     """This class manages DB storage for the HBnB clone project."""
@@ -27,12 +36,12 @@ class DBStorage:
         """This method initializes an instance of the
         DBStorage class.
         """
-        sqluser = os.getenv("HBNB_MYSQL_USER")
-        sqlpass = os.getenv("HBNB_MYSQL_PWD")
-        sqlhost = os.getenv("HBNB_MYSQL_HOST")
-        sqldbname = os.getenv("HBNB_MYSQL_DB")
+        sr = os.getenv("HBNB_MYSQL_USER")
+        sp = os.getenv("HBNB_MYSQL_PWD")
+        sh = os.getenv("HBNB_MYSQL_HOST")
+        sd = os.getenv("HBNB_MYSQL_DB")
         self.__engine = create_engine(
-            "mysql+mysqldb://{}:{}@{}/{}".format(sqluser, sqlpass, sqlhost, sqldbname),
+            "mysql+mysqldb://{}:{}@{}/{}".format(sr, sp, sh, sd),
             pool_pre_ping=True,
         )
         if os.getenv("HBNB_ENV") == "test":
@@ -46,12 +55,14 @@ class DBStorage:
             cls(Class Obj): Class that the returned dictionary
                 will contain instances of.
         """
-        if cls is None:
-            myObjects = []
-            for mClass in [User, State, City, Amenity, Place, Review]:
-                myObjects.extend(self.__session.query(mClass).all())
-        else:
-            myObjects = self.__session.query(cls).all()
+        myDict = {}
+        for Class in MyClasses:
+            if cls is None or cls is MyClasses[Class] or cls is Class:
+                results = self.__session.query(MyClasses[Class]).all()
+                for obj in results:
+                    key = obj.__class__.__name__ + "." + obj.id
+                    myDict[key] = obj
+        return myDict
 
         rDict = {"{}.{}".format(type(obj).__name__, obj.id): obj for obj in myObjects}
         return rDict
@@ -76,14 +87,6 @@ class DBStorage:
         """Creates all tables in the database & creates the current
         database session
         """
-        from models.base_model import Base
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
-
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
