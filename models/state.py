bec33465ff7cd44e9bc1ed52b76872888fc40d32
El-sharqawy <elsharqawy2@gmail.com>
2024-05-18 19:54:54 +0300

    New Engine

--- models/state.py
+++ models/state.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""State Module for HBNB project"""
+
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """State class"""
+
     if models.storage_type == "db":
-        __tablename__ = 'states'
+        __tablename__ = "states"
         name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state', cascade='all, delete')
+        cities = relationship("City", backref="state", cascade="all, delete")
     else:
         name = ""
 
@@ -19,13 +22,17 @@ class State(BaseModel, Base):
         super().__init__(*args, **kwargs)
 
     if models.storage_type != "db":
+
         @property
         def cities(self):
             from models import storage
+
             citieslist = []
             for key, value in storage.all().items():
-                if type(value).__name__ == 'City':
-                    if ('state_id' in value.__dict__ and
-                            str(value.__dict__['state_id']) == self.id):
+                if type(value).__name__ == "City":
+                    if (
+                        "state_id" in value.__dict__
+                        and str(value.__dict__["state_id"]) == self.id
+                    ):
                         citieslist.append(value)
-            return (citieslist)
+            return citieslist
