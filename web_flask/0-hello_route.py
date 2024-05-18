713b1f7a30d847515937ebe8c35bfd651e3e7a80
El-sharqawy <elsharqawy2@gmail.com>
2024-05-18 14:59:20 +0000

    Hello HBNB!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,7 +6,8 @@ start Flask web Application
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/', strict_slashes = False)
+
+@app.route('/', strict_slashes=False)
 def hello_world():
     """Prints Hello HBNB!"""
     return 'Hello HBNB!'
