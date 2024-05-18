8e56d095f477e7f5d3ed3e99fc4e641d83791085
El-sharqawy <elsharqawy2@gmail.com>
2024-05-18 20:40:20 +0300

    New Engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,78 +1,82 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
+
 import json
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.review import Review
+from models.place import Place
+from models.state import State
+from models.user import User
 from hashlib import md5
 
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
-    __file_path = 'file.json'
+
+    __file_path = "file.json"
     __objects = {}
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls is not None:
             tempDict = {}
-            for key, value in FileStorage.__objects.items():
+            for key, value in self.__objects.items():
                 if value.__class__ == cls:
-                    if 'password' in value.__dict__:
+                    if "password" in value.__dict__:
                         hpwd = md5()
-                        hpwd.update(value.__dict__['password'].encode('utf-8'))
-                        value.__dict__['password'] = hpwd.hexdigest()
+                        hpwd.update(value.__dict__["password"].encode("utf-8"))
+                        value.__dict__["password"] = hpwd.hexdigest()
                     tempDict[key] = value
             return tempDict
         else:
-            return FileStorage.__objects
+            return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        self.all().update({obj.to_dict()["__class__"] + "." + obj.id: obj})
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
+        with open(self.__file_path, "w") as f:
             temp = {}
-            temp.update(FileStorage.__objects)
+            temp.update(self.__objects)
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
 
     def delete(self, obj=None):
-        """Deletes the Object from storage dictionary
-        """
+        """Deletes the Object from storage dictionary"""
         if obj is None:
             return
 
         temp = {}
-        temp.update(FileStorage.__objects)
+        temp.update(self.__objects)
         for key, val in temp.items():
             if val == obj:
-                del FileStorage.__objects[key]
+                del self.__objects[key]
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            "BaseModel": BaseModel,
+            "User": User,
+            "Place": Place,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Review": Review,
+        }
         try:
             temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
+            with open(self.__file_path, "r") as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val["__class__"]](**val)
         except FileNotFoundError:
             pass
 
     def close(self):
         """reloads and deserialize JSON file ot Objects"""
-        reload()
+        self.reload()
