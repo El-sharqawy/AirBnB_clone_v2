4702a9790fea1411dd282470a04c3204d1302181
El-sharqawy <elsharqawy2@gmail.com>
2024-05-18 19:55:47 +0300

    New Engine

--- models/state.py
+++ models/state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """State Module for HBNB project"""
 
-import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
